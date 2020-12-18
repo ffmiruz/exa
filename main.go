728d2b48cb032aa98cb01130462728745963725b
ffmiyo <leffmiyo@gmail.com>
2020-12-18 20:14:54 +0800

    refactor to handle key press event

--- main.go
+++ main.go
@@ -7,31 +7,45 @@ import (
 )
 
 func main() {
-
-	// Buffer to store input
-	var b []byte = make([]byte, 1)
-
 	oldState, err := term.MakeRaw(0)
 	if err != nil {
 		panic(err)
 	}
 	defer term.Restore(0, oldState)
 
-	for {
-		os.Stdin.Read(b)
+	// Buffer to store input
+	var b []byte = make([]byte, 1)
 
-		// ASCII 17 (CTRL + q) as quit -> b[0] == 17 .
-		// By design, CTRL+char ASCII value can be calculated by bitwise-AND
-		// binary 00011111 (0x1f) with char.
-		if b[0] == 0x1f&'q' {
-			break
-		}
-		// Skip control characters. ASCII codes 0–31 are all control characters.
-		// 127 is also a control character. 32–126 are all printable.
-		if b[0] < 32 || b[0] > 126 {
-			continue
-		}
+	for run := true; run; {
+		run = processKeyPress(b)
+	}
+}
+
+// Handle keypress event
+func processKeyPress(b []byte) bool {
+
+	switch ch := readKey(b); {
+
+	// ASCII 17 (CTRL + q) as quit -> b[0] == 17 .
+	// By design, CTRL+char ASCII value can be calculated by bitwise-AND
+	// binary 00011111 (0x1f) with char.
+	case ch == 0x1f&'q':
+		return false
+
+	// Skip control characters. ASCII codes 0–31 are all control characters.
+	// 127 is also a control character. 32–126 are all printable.
+	case ch < 32:
+		fallthrough
+	case ch == 127:
+		break
+	default:
 		fmt.Print(string(b))
 	}
+	return true
+}
 
+// Wait for a keypress and return its value
+func readKey(b []byte) rune {
+	os.Stdin.Read(b)
+	return rune(b[0])
 }
